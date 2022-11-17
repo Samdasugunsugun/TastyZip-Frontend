8cd30f942a4789f6d0d50d6594a5f50e36749cda
주영상 <jyns@juyeongsang-ui-MacBookAir.local>
2022-11-18 00:45:28 +0900

    feat: search enging 및 결과 컴포넌트 추가

---
+++ src/components/SearchEngins.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+import SearchEngine from '../components/SearchEngine.vue';
+import ResultView from './ResultView.vue';
+import { ref } from 'vue';
+import router from '../router';
+import itemResults from '../dummy';
+
+const keyword = ref('');
+const itemList = ref([]);
+
+function search() {
+  // TODO: axios call by keyword
+  itemList.value = itemResults;
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="keyword" placeholder="검색어를 입력해주세요" />
+    <button @click="search">검색</button>
+  </div>
+  <div>{{ keyword }}</div>
+  <ResultView :itemList="itemList" />
+</template>
