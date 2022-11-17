344a20a5b6cea938274a04db2d52ff062ad692ed
주영상 <jyns@juyeongsang-ui-MacBookAir.local>
2022-11-18 00:47:24 +0900

    feat: 더미 데이터 샘플 생성

---
+++ src/dummy.js
@@ -0,0 +1,20 @@
+const itemResults = [
+  {
+    title: '고대맛집',
+    location: '서울특별시 성북구 안암동',
+  },
+  {
+    title: '무르무르',
+    location: '서울특별시 성북구 안암동',
+  },
+  {
+    title: '유메노카츠',
+    location: '서울특별시 성북구 하월곡동',
+  },
+  {
+    title: '이정욱',
+    location: '서울특별시 성북구 안암동',
+  },
+];
+
+export default itemResults;
